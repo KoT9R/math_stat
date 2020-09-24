86981aba8586139a53196c0475b10ee4ac1514bc
KoT9R <vandal5310@gmail.com>
2020-09-24 16:08:03 +0300

    Add interval

--- KursWork/CramerCriterion.py
+++ KursWork/CramerCriterion.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 n = 50
 mu = 0
 sigma = 1
+low, high = -math.sqrt(3), math.sqrt(3)
 
 
-def cramerVonMisesTest(x, mu_estimate, sigma_estimate):
+def cramerVonMisesTest(x, a, b):
     A = np.ndarray(shape=(n, 1))
     B = np.ndarray(shape=(n, 1))
     C = np.ndarray(shape=(n, 1))
@@ -21,17 +22,38 @@ def cramerVonMisesTest(x, mu_estimate, sigma_estimate):
     H = np.ndarray(shape=(n, 1))
     I = np.ndarray(shape=(n, 1))
 
+    count1 = 0
+    count2 = 0
+    for j in range(n):
+        if (x[j] > b):
+            count1 = count1 + 1
+
+    for j in range(n):
+        if (x[j] < a):
+            count2 = count2 + 1
+    print("count1 = " + str(count1))
+    print("count2 = " + str(count2))
+
     for j in range(n):
         A[j] = (2 * j + 1) / (2 * n)
-        B[j] = st.norm.cdf(x[j], loc=mu_estimate, scale=sigma_estimate)
-        C[j] = math.log(B[j])
-        D[j] = A[j] * C[j]
-        E[j] = 1 - A[j]
-        F[j] = 1 - B[j]
-        G[j] = math.log(F[j])
-        H[j] = E[j] * G[j]
-        I[j] = D[j] + H[j]
 
+        if (x[j] < a):
+            B[j] = 0
+        else:
+            if (x[j] > b):
+                B[j] = 1
+            else:
+                B[j] = (x[j] - a) / (b - a)
+
+    C[j] = math.log(B[j])
+    D[j] = A[j] * C[j]
+    E[j] = 1 - A[j]
+    F[j] = 1 - B[j]
+    G[j] = math.log(F[j])
+    H[j] = E[j] * G[j]
+    I[j] = D[j] + H[j]
+
+    print(np.sum(I))
     stat = -n - 2 * np.sum(I)
 
     for j in range(n):
@@ -51,25 +73,27 @@ def cramerVonMisesTest(x, mu_estimate, sigma_estimate):
 
 
 def main():
-    x = np.random.normal(mu, sigma, n)
-    x = np.sort(x)
+    data = np.random.uniform(low, high, n)
+    data = np.sort(data)
 
-    count, bins, ignored = plt.hist(x, 30, density=True)
+    x_max = max(data)
+    x_min = min(data)
+    a = x_min - (x_max + x_min) / (n - 1)
+    b = x_max + (x_max - x_min) / (n - 1)
+    print("Оценки параметров:" + str("%.4f" % a), str("%.4f" % b))
+
+    count, bins, ignored = plt.hist(data, 30, density=True)
     plt.xlabel("")
     plt.ylabel("frequency")
     plt.show()
 
     for j in range(n):
         print("%s &" % str(j + 1),
-              "%s \\\\" % x[j],
+              "%s \\\\" % data[j],
               "\hline"
               )
 
-    mu_estimate = np.mean(x)
-    sigma_estimate = math.sqrt(statistics.variance(x))
-    print("Метод максимального правдоподобия:" + str("%.4f" % mu_estimate), str("%.4f" % sigma_estimate))
-
-    stat = cramerVonMisesTest(x, mu_estimate, sigma_estimate)
+    stat = cramerVonMisesTest(data, a, b)
     print("Значение статистики:" + str(stat))
 
 
